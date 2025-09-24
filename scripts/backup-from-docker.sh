814907a32a007783a3693552616fbf66d44f1a04
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-09-24 10:21:35 +0200

    Remove unused files

--- scripts/backup-from-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-F=~/backup/dora-ros/$(date +%FT%T).tar.gz ; docker exec -it great_elgamal bash -c $'tar -cxOC ~ $( printf -- \'--exclude=./dora-ros/%s\n\' ros2_ws/src/{build,log,install,rplidar_ros} .git) dora-ros | base64' | tr -d '\n\r' | base64 -d > $F ; file $F
