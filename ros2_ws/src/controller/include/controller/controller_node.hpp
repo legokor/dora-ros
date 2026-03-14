ebac951a243a17cc82df9a720cd02f5791efb56a
Szuromi Mihály <231324199+Tower695@users.noreply.github.com>
2026-03-14 19:42:10 +0100

    Odometry and odom transform implementation 1.0

--- ros2_ws/src/controller/include/controller/controller_node.hpp
+++ ros2_ws/src/controller/include/controller/controller_node.hpp
@@ -4,8 +4,13 @@
 #include "controller/uart_handler.hpp"
 
 #include "rclcpp/node.hpp"
+#include "tf2_ros/transform_broadcaster.hpp"
+#include "tf2/LinearMath/Quaternion.hpp"
 
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
 #include <thread>
 #include <atomic>
@@ -18,16 +23,27 @@ public:
     ~ControllerNode();
 
 private:
+
+    // Odometry data:
+    float x_total = 0;
+    float y_total = 0;
+    float th_total = 0;
+    rclcpp::Time last_time;
+
     UARTHandler uart;
 
     std::atomic<bool> io_thread_running;
     std::thread io_thread;
 
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr velocity_subscriber;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr imu_publisher;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher; // Eredetileg imu_publisher, de nem enkóderből mértet küld vissza?
+    std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
 
     void sendTwist(const geometry_msgs::msg::Twist::SharedPtr& msg);
     void handleReceivedMessage(const dora::ReceivedMessage& msg);
+    void odomUpdate(const dora::SpeedData& speedData);
+    void sendTransform(const rclcpp::Time& current_time);
+    void sendOdometry(const rclcpp::Time& current_time, const dora::SpeedData& speedData);
 };
 
 } // namespace dora
--- ros2_ws/src/controller/src/controller_node.cpp
+++ ros2_ws/src/controller/src/controller_node.cpp
@@ -10,9 +10,13 @@ using Twist = geometry_msgs::msg::Twist;
 using namespace dora;
 
 ControllerNode::ControllerNode() : Node("uart_handler_node"), uart("/dev/ttyUSB1", B115200) {
-    velocity_subscriber = create_subscription<Twist>("cmd_vel", 10, [this](Twist::SharedPtr t) { sendTwist(t); });
-    imu_publisher = create_publisher<Twist>("odom", 10);
 
+    // Initialising publishers and subscribers
+    velocity_subscriber = create_subscription<Twist>("cmd_vel", 10, [this](Twist::SharedPtr t) { sendTwist(t); });
+    odom_publisher = create_publisher<nav_msgs::msg::Odometry>("odom", 10);
+    tf_broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(*this);
+
+    // Starting IO thread
     io_thread = std::thread([this]() {
         while (io_thread_running) {
             std::expected<ReceivedMessage, std::string> msg = uart.receiveMessage();
@@ -24,6 +28,9 @@ ControllerNode::ControllerNode() : Node("uart_handler_node"), uart("/dev/ttyUSB1
         }
     });
 
+    // Starting measurement time
+    last_time = this->get_clock()->now();
+
     RCLCPP_INFO(get_logger(), "controller node initialized succesfully");
 }
 
@@ -33,19 +40,105 @@ void ControllerNode::sendTwist(const Twist::SharedPtr& msg) {
 
 void ControllerNode::handleReceivedMessage(const ReceivedMessage& msg) {
     if (std::holds_alternative<SpeedData>(msg)) {
-        auto [x, y, w] = std::get<SpeedData>(msg);
-
-        Twist twist;
-        twist.linear.x = x;
-        twist.linear.y = y;
-        twist.angular.z = w;
-        imu_publisher->publish(twist);
-
-        RCLCPP_INFO(get_logger(), "got speed data: x:%f y:%f w:%f", x, y, w);
+        odomUpdate(std::get<SpeedData>(msg));
     } else if (std::holds_alternative<RobotStatus>(msg)) {
         auto [voltage] = std::get<RobotStatus>(msg);
         RCLCPP_INFO(get_logger(), "got robot status: v:%f", voltage);
     }
+}
+
+void ControllerNode::odomUpdate(const SpeedData& speedData) {
+
+    // Getting the elapsed time from the last measurement
+    rclcpp::Time current_time = this->get_clock()->now();
+    double delta_time = (current_time - last_time).seconds();
+
+    // Extracting speed data
+    auto [vx, vy, vth] = speedData;
+
+    // Coordinate transform
+    double delta_th = vth * delta_time;
+    double mid_th = th_total + delta_th / 2.0; // The average of the time interval
+    double delta_x = (vx * cos(mid_th) - vy * sin(mid_th)) * delta_time;
+    double delta_y = (vx * sin(mid_th) + vy * cos(mid_th)) * delta_time;
+
+    x_total += delta_x;
+    y_total += delta_y;
+    th_total += delta_th;
+
+    // Sending data:
+    sendTransform(current_time);
+    sendOdometry(current_time, speedData);
+}
+
+void ControllerNode::sendTransform(const rclcpp::Time& current_time) {
+        // Initializing message
+        geometry_msgs::msg::TransformStamped msg;
+
+        // Setting header (Time is also important)
+        msg.header.stamp = current_time;
+        msg.header.frame_id = "world";
+        msg.child_frame_id = "base_footprint";
+
+        // Setting positional data
+        msg.transform.translation.x = x_total;
+        msg.transform.translation.y = y_total;
+        msg.transform.translation.z = 0.0;
+
+        // Rotational data
+        tf2::Quaternion q;
+        q.setRPY(0, 0, th_total);
+        msg.transform.rotation.x = q.x();
+        msg.transform.rotation.y = q.y();
+        msg.transform.rotation.z = q.z();
+        msg.transform.rotation.w = q.w();
+        
+        // Publishing message
+        tf_broadcaster->sendTransform(msg);
+
+        // Logging
+        std::stringstream ss;
+        ss << "Transformation:\n" <<
+            "- Pose: " << x_total << " ; " << y_total << " ; " << th_total << "\n";
+        RCLCPP_INFO(get_logger(), ss.str().c_str());
+}
+
+void ControllerNode::sendOdometry(const rclcpp::Time& current_time, const SpeedData& speedData) {
+        // Initializing message
+        nav_msgs::msg::Odometry msg;
+
+        // Setting header (Time is also important)
+        msg.header.stamp = current_time;
+        msg.header.frame_id = "world";
+        msg.child_frame_id = "base_footprint";
+
+        // Setting positional data
+        msg.pose.pose.position.x = x_total;
+        msg.pose.pose.position.y = y_total;
+        msg.pose.pose.position.z = 0;
+
+        // Rotational data
+        tf2::Quaternion q;
+        q.setRPY(0, 0, th_total);
+        msg.pose.pose.orientation = tf2::toMsg(q);
+
+        // Velocity data
+        auto [vx, vy, vth] = speedData;
+        msg.twist.twist.linear.x = vx;
+        msg.twist.twist.linear.y = vy;
+        msg.twist.twist.angular.z = vth;
+
+        // TODO: Covarience matrix if the data is noisy and Dora doesn't move smoothly
+        
+        // Publishing message
+        odom_publisher->publish(msg);
+
+        // Logging
+        std::stringstream ss;
+        ss << "Odometry:\n" <<
+            "- Pose: " << x_total << " ; " << y_total << " ; " << th_total << "\n" <<
+            "- Velocity: " << vx << " ; " << vy << " ; " << vth << "\n";
+        RCLCPP_INFO(get_logger(), ss.str().c_str());
 }
 
 ControllerNode::~ControllerNode() {
